ee7cd1b2d47c70b6f17f8b291dee7260e6ddef7b
Manuel Gonzales <mgonzalesaira@my.bcit.ca>
2015-03-02 23:13:53 -0800

    Added Client and Server pseudocode
    Version 1.0, will add stuff after discussing some points in class.

---
+++ extra/pseudocode/server.c
@@ -0,0 +1,104 @@
+// server
+
+Start Listening for Connections
+while
+{
+	if(there is a new client)
+	{
+		Create a new Session.
+		Add session to session List.
+	]
+}
+
+/**
+Interfaces directly with the client, one is created for each client. 
+This process is used to exchange control data with the client 
+@socket Client - New client connection
+**/
+Session(socket client)
+{
+	Send Client the list of all the sessions (Clients Connected)
+	
+	start Listening from the socket
+	while
+	{	
+		Check Client List.
+		if(there is an update)
+		{
+			Send update to every client
+		}
+		
+		Read 'flag' from the socket.
+		
+		if(flag == download request)
+		{
+			read from the socket
+			get the Song out of the message received
+			Call Upload(Song, client)
+		}		
+		else if(flag == stream request)
+		{
+			read from the socket
+			get the Song out of the message received
+			Add Song to Song Queue.
+		}
+		else if(flag == disconnect flag)
+		{
+			Remove client from client list
+			close socket and connection
+			free resources and exit the process
+		}
+		else
+		{
+			Flag Error
+			Let the client know
+		}
+	
+	}
+}
+
+
+/**
+Reads from the currently selected music file, and multicasts it on the network
+**/
+Stream()
+{
+	While 
+	{
+		if(there are Songs in the Queue)
+		{
+			Open the File(Song)
+			Start Playing Song in front of the Queue
+			//Let Client know the song being played
+			
+			For(every Client in client List)
+			{
+				Send the audio using UDP sockets for each client.
+			}
+			
+			if(Song is complete)
+			{
+				Close File
+				Remove Song from Queue
+			}
+		}	
+		
+	}
+	
+
+}
+
+
+
+/**
+Spawned whenever a session's client requests to download a song file. 
+It reads from the requested song file, and sends it to the requesting client
+@param Song   - piece of sound requested by the client
+@param Client - client with the request 
+**/
+Upload(string Song, Session client)
+{
+	Open the File(Song)
+	Send File to client using the TCP Socket.
+	Close File
+}
